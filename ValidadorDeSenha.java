7c23c050731e6874e2174886106c321c2058aa07
Cleyton Filipe <cleyton.sdv@gmail.com>
2023-11-06 21:57:25 -0300

    Versão final com código totalmente funcional, utilizando das bibliotecas Pattern para padronizar os conjuntos de caracteres; e a biblioteca Matcher para encontrar correspondências na string senhaUsuario. Foi adicionado novas variáveis com cada tipo de objeto para representar os conjuntos que devem estar na senha.

--- ValidadorDeSenha.java
+++ ValidadorDeSenha.java
@@ -1,57 +1,55 @@
 package validador;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class ValidadorDeSenha {
 
-	public static void main(String[] args) {
-		
-		String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
-		
-		boolean contemCaracter = false;
-		
-		Scanner tl = new Scanner(System.in);
-		System.out.println("Defina sua senha: \n"
-				+ "\n"
-				+ "Sua senha deve possuir de 6 à 16 caracteres e:\n"
-				+ "\n"
-				+ "Uma letra maiúscula\n"
-				+ "Uma letra minúscula\n"
-				+ "Um número\n"
-				+ "Um caractere especial (!@#$%^&*)\n"
-				+ "\n");
-		String senhaUsuario = tl.next();
-		tl.close();
-		System.out.println();
-		
-		if (senhaUsuario.length() >= 6 && senhaUsuario.length() <= 16 ) {
-			
-			for (int i = 0; i < caracteres.length(); i++) {
-				char caracter = caracteres.charAt(i);
-				
-				if (senhaUsuario.contains(String.valueOf(caracter))) {
-				contemCaracter = true;
-				break;
-				}
-				
-			}
-			
-			if (contemCaracter) {
-				System.out.println("A senha " + senhaUsuario + " é válida!");
-			} else {
-				System.out.println("A senha " + senhaUsuario + " não contém os caracteres necessários...");
-			}
-			
-		} else  if (senhaUsuario.length() < 6){
-			
-			System.out.println("Sua senha precisa de mais caracteres, digite pelo menos 6 caracteres");
-			
-		} else {
-			
-			System.out.println("Sua senha tem caracteres demais, elabore uma senha com até 16 caracteres :D");
-			
-		}
+    public static void main(String[] args) {
+        
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+        String maiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String minuscula = "abcdefghijklmnopqrstuvwxyz";
+        String numero = "0123456789";
+        String especial = "!@#$%^&*";
+        
+        Scanner tl = new Scanner(System.in);
+        System.out.println("Defina sua senha: \n"
+                + "\n"
+                + "Sua senha deve possuir de 6 à 16 caracteres e:\n"
+                + "\n"
+                + "Pelo menos uma letra maiúscula\n"
+                + "Pelo menos uma letra minúscula\n"
+                + "Pelo menos um número\n"
+                + "Pelo menos um caractere especial (!@#$%^&*)\n"
+                + "\n");
+        String senhaUsuario = tl.next();
+        tl.close();
+        System.out.println();
+        
+        if (senhaUsuario.length() >= 6 && senhaUsuario.length() <= 16) {
+            boolean possuiMaiuscula = verificaCaracteres(senhaUsuario, maiuscula);
+            boolean possuiMinuscula = verificaCaracteres(senhaUsuario, minuscula);
+            boolean possuiNumero = verificaCaracteres(senhaUsuario, numero);
+            boolean possuiEspecial = verificaCaracteres(senhaUsuario, especial);
+            
+            if (possuiMaiuscula && possuiMinuscula && possuiNumero && possuiEspecial) {
+                System.out.println("A senha " + senhaUsuario + " é válida!");
+            } else {
+                System.out.println("A senha " + senhaUsuario + " não atende aos critérios de validação.");
+            }
+        } else if (senhaUsuario.length() < 6) {
+            System.out.println("Sua senha precisa de mais caracteres, digite pelo menos 6 caracteres");
+        } else {
+            System.out.println("Sua senha tem caracteres demais, elabore uma senha com até 16 caracteres :D");
+        }
+    }
 
-	}
-
+    // Função para verificar se a senha contém pelo menos um conjunto de caracteres
+    public static boolean verificaCaracteres(String senha, String conjuntoCaracteres) {
+        Pattern pattern = Pattern.compile("[" + Pattern.quote(conjuntoCaracteres) + "]");
+        Matcher matcher = pattern.matcher(senha);
+        return matcher.find();
+    }
 }
